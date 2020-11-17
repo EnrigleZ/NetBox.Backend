5c5ff17774971f80af903aa706d36c60bd7b71ce
Yucong Zhou <v-yucoz@microsoft.com>
2020-11-17 16:23:12 +0800

    feat: test json response

--- TestAPI/models.py
+++ TestAPI/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.utils import timezone
+
+import uuid
 
 class TestStruct(models.Model):
   title = models.CharField(verbose_name='title', max_length=50)
   content = models.CharField(verbose_name='content', max_length=200)
+  created_at = models.DateTimeField(verbose_name='createTime', editable=False)
+  updated_at = models.DateTimeField(verbose_name='updateTime')
+  id = models.UUIDField(verbose_name='id', primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+  def save(self, *args, **kwargs):
+    timestamp = timezone.now()
+    if not self.id:
+      self.created_at = timestamp
+    self.updated_at = timestamp
+    return super().save(*args, **kwargs)
+    
