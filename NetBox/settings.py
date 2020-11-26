d7f5963f17bcdf3f37f3b9968b8ab25c6d640ec2
Yucong Zhou <v-yucoz@microsoft.com>
2020-11-26 20:03:59 +0800

    feat: add jwt auth package

--- NetBox/settings.py
+++ NetBox/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'NetBox.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 # Database
