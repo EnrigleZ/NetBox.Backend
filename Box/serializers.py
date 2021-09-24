c51acc3b1e82e47b72352cdb5c832e1010a17d9c
Yucong Zhou <zhouyucong0421@foxmail.com>
2021-09-24 19:57:19 +0800

    fix: add create method for serializer

--- Box/serializers.py
+++ Box/serializers.py
@@ -11,23 +11,30 @@ class BoxFileSerializer(serializers.ModelSerializer):
     created_at = TimestampField(read_only=True)
     updated_at = TimestampField(read_only=True)
     file_content = serializers.FileField(required=False)
-    box_file_area = serializers.CharField(source='box_file_area.id', default=None)
+    # box_file_area = serializers.CharField(source='box_file_area', default=None)
+
     class Meta:
         model = BoxFile
         fields = '__all__'
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
+        representation['exist'] = False
         if hasattr(instance, 'file_content'):
             try:
                 representation['size'] = instance.file_content.size
                 representation['path'] = instance.file_content.path
+                representation['exist'] = True
             except FileNotFoundError:
                 representation['size'] = 0
             except:
                 pass
 
         return representation
+
+    def create(self, validated_data):
+        print(validated_data)
+        return BoxFile.objects.create(**validated_data)
 
 # class BoxFileContentSerializer(serializers.ModelSerializer):
 #     class Meta:
