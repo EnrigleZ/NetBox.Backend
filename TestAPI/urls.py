38760f0ac730b45e4498fb07b737ccd551dcfe1a
Yucong Zhou <zhouyucong0421@foxmail.com>
2021-10-27 22:12:30 +0800

    feat: add test api return timestamp

--- TestAPI/urls.py
+++ TestAPI/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from TestAPI import views
+from TestAPI.views import defaultTest
 
 urlpatterns = [
   path('test-struct/', views.TestStructViewSet.as_view({'get': 'retrieve', 'post': 'create'})),
   path('test-structs/', views.TestStructViewSet.as_view({'get': 'list'})),
+  path('', defaultTest)
 ]
